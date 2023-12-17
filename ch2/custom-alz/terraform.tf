5b6860d22c869d48e4ae8fc5f8a308e324772b19
Ashish Raj <ashishrajsrivastava@yahoo.in>
2023-12-18 00:17:04 +0100

    added chapter 2 final

---
+++ ch2/custom-alz/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.74.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "iac-terraform-state-rg"
+    storage_account_name = "iacbookstate2023"
+    container_name       = "alzcoretfstate"
+    key                  = "prod.alz.terraform.tfstate"  
+  }
+}
+provider "azurerm" {
+  features {}
+}
---
+++ ch2/custom-alz/variables.tf
@@ -0,0 +1,9 @@
+variable "root_id" {
+  type    = string
+  default = "adp-alz"
+}
+
+variable "root_name" {
+  type    = string
+  default = "ADP ALZ"
+}
