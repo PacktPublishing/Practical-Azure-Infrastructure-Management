fb2ea398ad5248ccf759327ba7bd831c5589401a
Ashish Raj <ashishrajsrivastava@live.com>
2024-03-30 22:11:11 +0100

    ch8 done

---
+++ ch8/ansible-control-node/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.74.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "iac-terraform-state-rg"
+    storage_account_name = "iacbookstate2023"
+    container_name       = "alzcoretfstate"
+    key                  = "prod.ansible-2.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+}
+data "azurerm_client_config" "current" {}
